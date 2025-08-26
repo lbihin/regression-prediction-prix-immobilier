65b11656e55b4646a4857342f1b2a80297578de8
Laurent Bihin <laurent.bihin@gmail.com>
2025-08-26 21:24:55 +0200

    premier baseline modèle

--- utils.py
+++ utils.py
@@ -1,4 +1,26 @@
+import pickle
+
+
 RANDOM_STATE = 0
 TEST_SIZE = 0.2
 ALPHA = 0.02
 TARGET = "SalePrice"
+
+
+def sauvegarder_model(
+    model,
+    file_name: str = "../data/models/best-model",
+    timestamp=None,
+    only_latest: bool = False,
+):
+    if not only_latest:
+        with open(f"{file_name}-{str(timestamp)}.pkl", "wb") as f:
+            pickle.dump(model, f)
+    with open(f"{file_name}-latest.pkl", "wb") as f:
+        pickle.dump(model, f)
+
+
+def ouvrir_model(file_name: str = "../data/models/best-model-latest.pkl"):
+    with open(file_name, "rb") as f:
+        clf = pickle.load(f)
+    return clf
